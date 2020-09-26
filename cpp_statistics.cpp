76ff066f8b7f0d31b4952998c01ea900d63dca68
ed4st <61949882+ed4st@users.noreply.github.com>
2020-09-26 00:08:51 -0500

    Initial local repo commit

---
+++ cpp_statistics.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+namespace stats{
+    struct vector{
+        int len; //array's length
+    };
+}
+
+int main(){
+    cout<<"hola mundo";
+    return 0;
+}
